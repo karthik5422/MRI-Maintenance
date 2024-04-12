91ac1b9a1c472f1153592c221d3a80a19b56192d
Vidura Fernando <heyvidura@gmail.com>
2024-04-12 10:39:38 +0530

    Update 2_MRI_Maintenance.py

--- pages/2_MRI_Maintenance.py
+++ pages/2_MRI_Maintenance.py
@@ -1,7 +1,6 @@
 import time
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 
 from mri_dt import mri_classifier_model
 from gen_resp import generate_response
@@ -11,20 +10,19 @@ st.subheader("Current Status & Actions for Maintenance: Based on Scanned Data")
 
 mri_classifier_model()
 
-# Read the data from the file
 data = []
 with open('output.csv', 'r') as f:
-    next(f)  # Skip the header row
+    next(f)  
     for l in f:
         line = l.strip().split(',')
         data.append(line)
 
-# Display data for the selected scan
+
 total_scans = len(data)
 selected_scan = st.slider("Select Scan", 1, total_scans, 1)
 line = data[selected_scan - 1]
 
-st.write(f"**Scan {selected_scan}**")
+st.write(f"**Current scan details with MRI performence data** {selected_scan}")
 columns = ['sl.no', 'scan_type', 'scan_time', 'snr (dB)', 'drift (Hz)', 'drift_ppm(ppm)', 'grad_perf(G/cm/ms)', 'coil_type', 'error_temp (°C)', 'sys_temp (°C)', 'cyro_boiloff(liters/hour)', 'rf_power (%)', 'grad_temp (°C)', 'grad_current (A)', 'x_axis_pos (mm)', 'y_axis_pos (mm)', 'z_axis_pos (mm)', 'Error_Code']
 df = pd.DataFrame([line], columns=columns)
 st.table(df)
