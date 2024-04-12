7d88a704c81cb6c4d0041533a8bf9531ce4fed18
Vidura Fernando <heyvidura@gmail.com>
2024-04-12 11:58:14 +0530

    fix: replacing the current content, while reading each row, added 10 sec delay

--- pages/2_MRI_Maintenance.py
+++ pages/2_MRI_Maintenance.py
@@ -12,27 +12,40 @@ mri_classifier_model()
 
 data = []
 with open('output.csv', 'r') as f:
-    next(f)  
+    next(f) 
     for l in f:
         line = l.strip().split(',')
         data.append(line)
 
-
 total_scans = len(data)
-selected_scan = st.slider("Select Scan", 1, total_scans, 1)
-line = data[selected_scan - 1]
 
-st.write(f"**Current scan details with MRI performence data** {selected_scan}")
-columns = ['sl.no', 'scan_type', 'scan_time', 'snr (dB)', 'drift (Hz)', 'drift_ppm(ppm)', 'grad_perf(G/cm/ms)', 'coil_type', 'error_temp (°C)', 'sys_temp (°C)', 'cyro_boiloff(liters/hour)', 'rf_power (%)', 'grad_temp (°C)', 'grad_current (A)', 'x_axis_pos (mm)', 'y_axis_pos (mm)', 'z_axis_pos (mm)', 'Error_Code']
-df = pd.DataFrame([line], columns=columns)
-st.table(df)
 
-st.write("**Current Status:**")
-error_code = line[-1]
-if error_code != "No Error":
-    llm_output = generate_response(line)
-    st.write("**There is an error found while scanning. The below actions are recommended.**")
-    st.write(llm_output)
-else:
-    st.write("**Scan is successful, MRI performance is normal**")
-st.divider()
+def update_data():
+    selected_scan_index = 0  
+
+    placeholder = st.empty()  
+
+    while True:
+        line = data[selected_scan_index]
+
+        with placeholder.container(): 
+            st.write(f"**Current scan details with MRI performence data** {selected_scan_index + 1}")
+            columns = ['sl.no', 'scan_type', 'scan_time', 'snr (dB)', 'drift (Hz)', 'drift_ppm(ppm)', 'grad_perf(G/cm/ms)', 'coil_type', 'error_temp (°C)', 'sys_temp (°C)', 'cyro_boiloff(liters/hour)', 'rf_power (%)', 'grad_temp (°C)', 'grad_current (A)', 'x_axis_pos (mm)', 'y_axis_pos (mm)', 'z_axis_pos (mm)', 'Error_Code']
+            df = pd.DataFrame([line], columns=columns)
+            st.table(df)
+
+            st.write("**Current Status:**")
+            error_code = line[-1]
+            if error_code != "No Error":
+                llm_output = generate_response(line)
+                st.write("**There is an error found while scanning. The below actions are recommended.**")
+                st.write(llm_output)
+            else:
+                st.write("**Scan is successful, MRI performance is normal**")
+            st.divider() # remove if needed idk
+
+        selected_scan_index = (selected_scan_index + 1) % total_scans
+        time.sleep(10)  
+
+
+update_data()
